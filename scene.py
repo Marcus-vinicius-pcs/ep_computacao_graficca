026630f433aa24a357a33922abb730c669379eb0
Marcus Vinicius P C Santos <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2024-06-09 20:19:31 -0300

    renderização de cubo seguindo tutorial

---
+++ scene.py
@@ -0,0 +1,25 @@
+from model import *
+import glm
+
+
+class Scene:
+    def __init__(self, app):
+        self.app = app
+        self.objects = []
+        self.load()
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def load(self):
+        app = self.app
+        add = self.add_object
+        
+        n, s = 30, 3
+        for x in range(-n, n, s):
+            for z in range(-n, n, s):
+                add(Cube(app, pos=(x, -s, z)))
+
+    def render(self):
+        for obj in self.objects:
+            obj.render()
